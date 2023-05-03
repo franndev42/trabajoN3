0879c0ddc49af59055255ce79518bf160a827f7c
Fran Garcia <fbranca2003@gmail.com>
2023-05-03 19:07:42 -0300

    se agrega el proyecto 3

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,7 @@ AOS.init();
 
 let menu_bar = document.getElementById('menu_bar');
 let menu = document.getElementById('menu');
-menu_bar.addEventListener('click', function () {
+
+menu_bar.addEventListener('click' , function (){
     menu.classList.toggle('show-menu')
-})
+} )
